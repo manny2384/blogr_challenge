b94456de72eddc44a9a45ab6490a1c0824474fec
manny2384 <gonzalezeh2015@gmail.com>
2021-04-04 12:32:47 -0700

    mobile menu draft

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ window.onload = () =>{
     let show_mobile_menu = true;
     document.getElementById('mobile_icon_nav').onclick = function(){
         let status = "none";
-        if(show_mobile_menu) status = "grid";
+        let symbol = "&#9776;";
+        if(show_mobile_menu){
+            status = "grid";
+            symbol = "&#10005;";
+        }
         show_mobile_menu = !show_mobile_menu;
-
+        document.getElementById('mobile_icon_nav').innerHTML = symbol;
         document.getElementsByClassName('div_nav_list')[0].style.display = status;
-   
+
     };
+
+
 }
